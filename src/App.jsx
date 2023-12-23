1cb350c9f7638b122b76ab14d534e9f02472ef67
githubdmn <dmninbox@gmail.com>
2023-12-23 16:20:23 +0100

    basic jsx - loop

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,29 @@
 
 
 function App() {
-  return (<h1> App functional component </h1>);
+  
+  const title = 'The Blog Post';
+  const body = 'This is my blog post';
+  const comments = [
+    { id: 1, text: 'Comment one' },
+    { id: 2, text: 'Comment two' },
+    { id: 3, text: 'Comment three' },
+  ];
+  
+
+  return (
+    <div className='container'> 
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+      <div className='comments'>
+        <h3> Comments ({comments.length}) </h3>
+        <ul> 
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>);
 }
 
 export default App;
