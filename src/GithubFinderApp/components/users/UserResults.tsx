34290b8e72277ad76825d9c64a37fb7069e57a82
githubdmn <dmninbox@gmail.com>
2024-10-08 13:10:55 +0200

    user list component fetch

--- src/GithubFinderApp/components/users/UserResults.tsx
+++ src/GithubFinderApp/components/users/UserResults.tsx
@@ -1,9 +1,39 @@
-
+import { useEffect, useState } from 'react';
 
 function UserResults() {
-  return (
-    <div>UserResults</div>
-  )
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(
+      `${import.meta.env.VITE_GITHUB_API_URL}/users`,
+      {
+        method: 'GET',
+        headers: {
+          // Authorization: `token ${import.meta.env.VITE_GITHUB_BEARER_TOKEN}`,
+        },
+      },
+    );
+    const data = await response.json();
+    setUsers(data);
+    setIsLoading(false);
+  };
+
+  if (!isLoading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => {
+          return <div key={user.id}>{user.login}</div>;
+        })}
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 }
 
 export default UserResults;
