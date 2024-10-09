2b29ec87baea8cd44be588d50aac0fdf340bdfe6
githubdmn <dmninbox@gmail.com>
2024-10-09 16:37:56 +0200

    Refactored to Context

--- src/GithubFinderApp/components/users/UserResults.tsx
+++ src/GithubFinderApp/components/users/UserResults.tsx
@@ -1,31 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
-import UserType from '../../types/UserType';
+import GithubContext from '../../context/GithubContext';
+
 
 function UserResults() {
-  const [users, setUsers] = useState<UserType[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
 
+  const { users, isLoading, fetchUsers } = useContext(GithubContext);
+  
   useEffect(() => {
     fetchUsers();
   }, []);
 
-  const fetchUsers = async () => {
-    const response = await fetch(
-      `${import.meta.env.VITE_GITHUB_API_URL}/users`,
-      {
-        method: 'GET',
-        headers: {
-          // Authorization: `token ${import.meta.env.VITE_GITHUB_BEARER_TOKEN}`,
-        },
-      },
-    );
-    const data = await response.json();
-    setUsers(data);
-    setIsLoading(false);
-  };
-
+  
+ 
   if (!isLoading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
